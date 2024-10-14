53f4c82021b3d01b95fc31e78f2abb6288c926bb
Popugairulat <150455922+Popugairulat@users.noreply.github.com>
2024-10-14 15:15:28 +0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <SFML/Graphics.hpp>
+
+void updatePlayerPosition(sf::RectangleShape& player);
+sf::RectangleShape createPlayer();
+
+#endif // FUNCTIONS_H
