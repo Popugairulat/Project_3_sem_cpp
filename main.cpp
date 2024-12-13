e1bcd150bb2f30a1e901b82dcbba19c33acf1005
Coopol <146334266+Coopol@users.noreply.github.com>
2024-12-13 22:23:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
     int WIDTH = static_cast<const int>(desktop.width * 0.9);
     const int HEIGHT = (WIDTH * 943 - WIDTH * 943 % 1880) / 1880;
     //int WIDTH = 1880, HEIGHT = 943;
-   
+
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "XY");
     GameState currentState = GameState::Start;
     // ЗАГРУЗКА ТЕКСТУР
@@ -26,6 +26,7 @@ int main() {
     textureManager.loadTexture("Stop", (folder / "button_stop.png").string());
     textureManager.loadTexture("Back", (folder / "button_back.png").string());
     textureManager.loadTexture("Question", (folder / "button_question.png").string());
+
     textureManager.loadTexture("Triangle", (folder / "Coin_3.png").string());
     textureManager.loadTexture("Square", (folder / "Coin_4.png").string());
     textureManager.loadTexture("Pentagon", (folder / "Coin_5.png").string());
@@ -37,55 +38,20 @@ int main() {
 
     //массивы с кнопками
     std::vector<Button> buttons_start;
-    buttons_start.emplace_back( WIDTH/2- WIDTH / 8, HEIGHT/2+ HEIGHT / 14, WIDTH/4, HEIGHT/7, "Start new game");
-    buttons_start.emplace_back(WIDTH / 2 - WIDTH / 8, HEIGHT / 2 +3* HEIGHT / 14, WIDTH / 4, HEIGHT / 7, "Continue the game");
+    buttons_start.emplace_back(WIDTH / 2 - WIDTH / 8, HEIGHT / 2 + HEIGHT / 14, WIDTH / 4, HEIGHT / 7, "Start new game");
+    buttons_start.emplace_back(WIDTH / 2 - WIDTH / 8, HEIGHT / 2 + 3 * HEIGHT / 14, WIDTH / 4, HEIGHT / 7, "Continue the game");
     buttons_start.emplace_back(WIDTH - WIDTH / 20 - WIDTH / 200, WIDTH / 200, WIDTH / 20, WIDTH / 20, "Settings");
     buttons_start.emplace_back(WIDTH - 2 * (WIDTH / 20 + WIDTH / 200), WIDTH / 200, WIDTH / 20, WIDTH / 20, "Question");
     buttons_start.emplace_back(WIDTH - 3 * (WIDTH / 20 + WIDTH / 200), WIDTH / 200, WIDTH / 20, WIDTH / 20, "Back");
-  
-    int x = WIDTH / 7;
-    int y = HEIGHT / 7;
-    int Coord[32][2] = { {x, y},{2 * x, y + 10},{3 * x, y + 20}, {4 * x, y + 30},{5 * x, y + 40},{6 * x, y + 50},{7 * x, y + 60},{8 * x, y + 70},
-    {8 * x, 2 * y},{7 * x, 2 * y + 10},{6 * x, 2 * y + 20},{5 * x, 2 * y + 30},{4 * x, 2 * y + 40},{3 * x, 2 * y + 50},{2 * x, 2 * y + 60}, {x, 2 * y + 70},
-    {x, 3 * y},{2 * x, 3 * y + 10},{3 * x, 3 * y + 20}, {4 * x, 3 * y + 30},{5 * x, 3 * y + 40},{6 * x, 3 * y + 50},{7 * x, 3 * y + 60},{8 * x, 3 * y + 70},
-    {8 * x, 4 * y},{7 * x, 4 * y + 10},{6 * x, 4 * y + 20},{5 * x, 4 * y + 30},{4 * x, 4 * y + 40},{3 * x, 4 * y + 50},{2 * x, 4 * y + 60}, {x, 4 * y + 70} };
 
-    // Заведем переменные
-
-    std::vector<Coin> All_Coins;
-    for (int i = 0; i < 8; i++)
-    {
-        int res = i / 2;
-        All_Coins.emplace_back("Triangle", res, Coord[i][0], Coord[i][1]);
-    }
-    for (int i = 8; i < 16; i++)
-    {
-        int res = i / 2;
-        All_Coins.emplace_back("Square", res, Coord[i][0], Coord[i][1]);
-    }
-    for (int i = 16; i < 24; i++)
-    {
-        int res = i / 2;
-        All_Coins.emplace_back("Pentagon", res, Coord[i][0], Coord[i][1]);
-    }
-    for (int i = 24; i < 32; i++)
-    {
-        int res = i / 2;
-        All_Coins.emplace_back("Hexagon", res, Coord[i][0], Coord[i][1]);
-    }
-
-    Player My_Player;
-    Player Other_Player;
-
-    Submarine Submarine;
 
     std::vector<Button> buttons_settings;
     buttons_settings.emplace_back(WIDTH / 2 - 100.0, 150.0, 200, 50, "Back");
-    buttons_settings.emplace_back( WIDTH/2-100.0, 250.0, 200, 50, "Back");
-    buttons_settings.emplace_back( WIDTH/2-100.0, 350.0, 200, 50, "Back");
-    buttons_settings.emplace_back( WIDTH/2-100.0, 450.0, 200, 50, "Back");
-    buttons_settings.emplace_back( WIDTH/2-100.0, 550.0, 200, 50, "Back");
-    buttons_settings.emplace_back( WIDTH-100.0, 0.0, 100, 100, "Back");
+    buttons_settings.emplace_back(WIDTH / 2 - 100.0, 250.0, 200, 50, "Back");
+    buttons_settings.emplace_back(WIDTH / 2 - 100.0, 350.0, 200, 50, "Back");
+    buttons_settings.emplace_back(WIDTH / 2 - 100.0, 450.0, 200, 50, "Back");
+    buttons_settings.emplace_back(WIDTH / 2 - 100.0, 550.0, 200, 50, "Back");
+    buttons_settings.emplace_back(WIDTH - 100.0, 0.0, 100, 100, "Back");
 
     std::vector<Button> buttons_game;
     buttons_game.emplace_back(WIDTH - WIDTH / 20 - WIDTH / 200, WIDTH / 200, WIDTH / 20, WIDTH / 20, "Settings");
@@ -95,36 +61,41 @@ int main() {
     while (window.isOpen()) {
         sf::Event event;
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {}
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-
-            }    
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    // Добавляем новый Ripple на позицию курсора
+                    ripples.emplace_back(sf::Vector2f(event.mouseButton.x, event.mouseButton.y));
+                }
+            }
         }
-       
+
+
         // Отрисовка в зависимости от текущего состояния
         if (currentState == GameState::Start) {
             renderStart(window);
 
             for (auto& button : buttons_start) {
-            button.draw_button(window, textureManager);
-            button.get_pressed(event);
-        }
-            if(buttons_start[0].pressed){
-                if(currentState == GameState::Start){
+                button.draw_button(window, textureManager);
+                button.get_pressed(event);
+            }
+            if (buttons_start[0].pressed) {
+                if (currentState == GameState::Start) {
                     currentState = GameState::Game;
                     buttons_start[0].pressed = false;
                 }
             }
-            if(buttons_start[1].pressed){
-                if(currentState == GameState::Start){
+            if (buttons_start[1].pressed) {
+                if (currentState == GameState::Start) {
                     currentState = GameState::Game;
                     buttons_start[1].pressed = false;
                 }
             }
-            if(buttons_start[2].pressed){
-                if(currentState == GameState::Start){
+            if (buttons_start[2].pressed) {
+                if (currentState == GameState::Start) {
                     currentState = GameState::Settings;
                     buttons_start[2].pressed = false;
                 }
@@ -139,64 +110,56 @@ int main() {
                 if (currentState == GameState::Start) {
                     currentState = GameState::Settings;
                     buttons_start[4].pressed = false;
-
-            }  
-            if (event.type == sf::Event::MouseButtonPressed) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    // Добавляем новый Ripple на позицию курсора
-                    ripples.emplace_back(sf::Vector2f(event.mouseButton.x, event.mouseButton.y));
-
                 }
             }
 
         }
-
 
         else if (currentState == GameState::Settings) {
 
             renderGame(window);
 
             for (auto& button : buttons_settings) {
-            button.draw_button(window, textureManager);
-            button.get_pressed(event);
-        }
-            if(buttons_settings[0].pressed){
-                if(currentState == GameState::Settings){
+                button.draw_button(window, textureManager);
+                button.get_pressed(event);
+            }
+            if (buttons_settings[0].pressed) {
+                if (currentState == GameState::Settings) {
                     currentState = GameState::Game;
                     buttons_settings[0].pressed = false;
                 }
             }
-            if(buttons_settings[1].pressed){
-                if(currentState == GameState::Settings){
+            if (buttons_settings[1].pressed) {
+                if (currentState == GameState::Settings) {
                     currentState = GameState::Game;
                     buttons_settings[1].pressed = false;
                 }
             }
-            if(buttons_settings[2].pressed){
-                if(currentState == GameState::Settings){
+            if (buttons_settings[2].pressed) {
+                if (currentState == GameState::Settings) {
                     currentState = GameState::Start;
                     buttons_settings[2].pressed = false;
                 }
             }
-            if(buttons_settings[3].pressed){
-                if(currentState == GameState::Settings){
+            if (buttons_settings[3].pressed) {
+                if (currentState == GameState::Settings) {
                     currentState = GameState::Level;
                     buttons_settings[3].pressed = false;
                 }
             }
-            if(buttons_settings[4].pressed){
-                if(currentState == GameState::Settings){
+            if (buttons_settings[4].pressed) {
+                if (currentState == GameState::Settings) {
                     currentState = GameState::Rating;
                     buttons_settings[4].pressed = false;
                 }
             }
-            if(buttons_settings[5].pressed){
-                if(currentState == GameState::Settings){
+            if (buttons_settings[5].pressed) {
+                if (currentState == GameState::Settings) {
                     currentState = GameState::Game;
                     buttons_settings[5].pressed = false;
                 }
             }
-        }  
+        }
 
         else if (currentState == GameState::Level) {
             renderGame(window);
@@ -210,7 +173,6 @@ int main() {
             }
             drawImage(window, "Player", Coord[My_Player.get_Index()][0], Coord[My_Player.get_Index()][1], x/2, x/2, textureManager);
             drawImage(window, "Player", Coord[Other_Player.get_Index()][0] + x/10, Coord[Other_Player.get_Index()][0], 100, 100, textureManager);
-
             for (auto& button : buttons_game) {
                 button.draw_button(window, textureManager);
                 button.get_pressed(event);
@@ -236,9 +198,8 @@ int main() {
         }
 
         window.display();
-     }
+    }
 
 
     return 0;
 }
-
