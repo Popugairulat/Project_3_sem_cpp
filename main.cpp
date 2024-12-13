7438f1374bfc9aea5e548d481466457c69a14464
Coopol <146334266+Coopol@users.noreply.github.com>
2024-12-13 22:51:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,42 @@ int main() {
     int WIDTH = static_cast<const int>(desktop.width * 0.9);
     const int HEIGHT = (WIDTH * 943 - WIDTH * 943 % 1880) / 1880;
     //int WIDTH = 1880, HEIGHT = 943;
+    int x = WIDTH / 7;
+int y = HEIGHT / 7;
+int Coord[32][2] = { {x, y},{2 * x, y + 10},{3 * x, y + 20}, {4 * x, y + 30},{5 * x, y + 40},{6 * x, y + 50},{7 * x, y + 60},{8 * x, y + 70},
+{8 * x, 2 * y},{7 * x, 2 * y + 10},{6 * x, 2 * y + 20},{5 * x, 2 * y + 30},{4 * x, 2 * y + 40},{3 * x, 2 * y + 50},{2 * x, 2 * y + 60}, {x, 2 * y + 70},
+{x, 3 * y},{2 * x, 3 * y + 10},{3 * x, 3 * y + 20}, {4 * x, 3 * y + 30},{5 * x, 3 * y + 40},{6 * x, 3 * y + 50},{7 * x, 3 * y + 60},{8 * x, 3 * y + 70},
+{8 * x, 4 * y},{7 * x, 4 * y + 10},{6 * x, 4 * y + 20},{5 * x, 4 * y + 30},{4 * x, 4 * y + 40},{3 * x, 4 * y + 50},{2 * x, 4 * y + 60}, {x, 4 * y + 70} };
 
+// Заведем переменные
+
+std::vector<Coin> All_Coins;
+for (int i = 0; i < 8; i++)
+{
+    int res = i / 2;
+    All_Coins.emplace_back("Triangle", res, Coord[i][0], Coord[i][1]);
+}
+for (int i = 8; i < 16; i++)
+{
+    int res = i / 2;
+    All_Coins.emplace_back("Square", res, Coord[i][0], Coord[i][1]);
+}
+for (int i = 16; i < 24; i++)
+{
+    int res = i / 2;
+    All_Coins.emplace_back("Pentagon", res, Coord[i][0], Coord[i][1]);
+}
+for (int i = 24; i < 32; i++)
+{
+    int res = i / 2;
+    All_Coins.emplace_back("Hexagon", res, Coord[i][0], Coord[i][1]);
+}
+
+Player My_Player;
+Player Other_Player;
+
+Submarine Submarine;
+    
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "XY");
     GameState currentState = GameState::Start;
     // ЗАГРУЗКА ТЕКСТУР
